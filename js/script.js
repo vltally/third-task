e405d6ab0f6173695ea49333bd01d6d057f0099b
Vitaliy <vitalik.koliesnikov@gmail.com>
2023-03-21 22:57:15 +0200

    advertisement and main settings were added

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const advertisement_btn = document.querySelector(".close-btn");
+const advertisement = document.querySelector(".advertisement");
+advertisement_btn.addEventListener("click",function(){
+    advertisement.style.display = 'none';
+});
