b1964437e0ef000cbc8bfcf53217c40073d5b3d2
Vitaliy <vitalik.koliesnikov@gmail.com>
2023-04-02 23:14:14 +0300

    navbar and pixel perfect for first 4 sectinos were done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 const advertisement_btn = document.querySelector(".close-btn");
 const advertisement = document.querySelector(".advertisement");
+const header = document.querySelector(".header");
+
 advertisement_btn.addEventListener("click",function(){
     advertisement.style.display = 'none';
+    header.classList.add('top');
+    document.querySelector(".browse-bands").classList.add('top');
 });
+
+
+const iconMenu = document.querySelector('.burger-icon');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function () {
+        document.body.classList.toggle('_lock');   
+        iconMenu.classList.toggle('_active');
+        header.classList.toggle('_active');
+        header.classList.add('top');
+    });
+}
+
